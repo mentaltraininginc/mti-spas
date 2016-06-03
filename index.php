548eeddabbe729aab5b3a170cf89482a19d03dc5
Erik Neff <eneff@primitivespark.com>
2016-06-03 15:07:53 -0700

    initial commit to get it up and at them

---
+++ index.php
@@ -0,0 +1,16 @@
+<html><!--test-->
+	<head>
+		<title>MentalApp</title>
+	<script type="text/javascript">
+
+	window.location = "app/";
+
+	</script>
+
+	</head>
+	<body>
+			
+
+
+	</body>
+</html>
