f265f995aaa6773336dc0c129dee2712fe43991e
Erik Neff <eneff@primitivespark.com>
2016-06-05 07:52:55 -0700

    comment out quoted_printable_encode

--- web/robertneff-com/includes/vcard.class.php
+++ web/robertneff-com/includes/vcard.class.php
@@ -33,6 +33,7 @@ function escape($string) {
 	return str_replace(';',"\;",$string);
 }
 
+/*
 // taken from PHP documentation comments
 function quoted_printable_encode($input, $line_max = 76) {
 	$hex 		= array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F');
@@ -72,6 +73,7 @@ function quoted_printable_encode($input, $line_max = 76) {
 	
 	return trim($output);
 }
+*/
 
 class vCard {
 	var $properties;
